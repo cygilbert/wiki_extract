c0b3eef8d5ae8327808b7b83cb682c2f44f34105
Your Name <you@example.com>
2019-09-17 17:18:44 +0200

    Add get_wikidump_url

--- modules/download_functions.py
+++ modules/download_functions.py
@@ -2,6 +2,16 @@
 Download functions
 """
 
+import requests
+from bs4 import BeautifulSoup
 
-def get_wikidump_url():
-    return True
+
+def get_wikidump_url(base_url="https://dumps.wikimedia.org/enwiki/"):
+    index = requests.get(base_url).text
+    # get the latest version of the dump
+    soup_index = BeautifulSoup(index, 'html.parser')
+    dumps_list = [
+        a['href'] for a in soup_index.find_all('a')
+        if a.has_attr('href')
+    ]
+    return base_url + dumps_list[-2]
