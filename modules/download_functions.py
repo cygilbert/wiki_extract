00b3126f14cf6f64f2ed5fc4116c3a6d28acd29c
Your Name <you@example.com>
2019-09-18 11:22:33 +0200

    Add get_list_downloads_wikidump and test

--- modules/download_functions.py
+++ modules/download_functions.py
@@ -6,10 +6,13 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def get_soup_from_url(url="https://en.wikipedia.org/wiki/English_Wikipedia"):
+    index = requests.get(url).text
+    return BeautifulSoup(index, 'html.parser')
+
+
 def get_wikidump_url(base_url="https://dumps.wikimedia.org/enwiki/"):
-    index = requests.get(base_url).text
-    # get the latest version of the dump
-    soup_index = BeautifulSoup(index, 'html.parser')
+    soup_index = get_soup_from_url(base_url)
     dumps_list = [
         a['href'] for a in soup_index.find_all('a')
         if a.has_attr('href')
@@ -17,7 +20,17 @@ def get_wikidump_url(base_url="https://dumps.wikimedia.org/enwiki/"):
     return base_url + dumps_list[-2]
 
 
-def get_list_url_wikidump(
+def get_list_downloads_wikidump(
         dump_url="https://dumps.wikimedia.org/enwiki/20190901/"
         ):
-    return [True]
+    soup_dump = get_soup_from_url(dump_url)
+    files = []
+    for file in soup_dump.find_all('li', {'class': 'file'}):
+        text = file.text
+        url = text.split()[0]
+        # Select the relevant files
+        if ('pages-articles' in url)\
+            and ('xml-p' in url)\
+                and ('multistream' not in url):
+            files.append((url, text.split()[1:]))
+    return files
--- test_functions.py
+++ test_functions.py
@@ -2,18 +2,56 @@
 Unit tests for the project
 """
 
-from modules.download_functions import get_wikidump_url, get_list_url_wikidump
+from modules.download_functions import get_wikidump_url,\
+    get_list_downloads_wikidump, get_soup_from_url
 
 
 class TestDownloadFunctions:
 
+    def test_get_soup_from_url(self):
+        soup = get_soup_from_url()
+        assert soup.contents
+
     def test_get_wikidump_url(self):
         dump_url = get_wikidump_url()
-        assert (dump_url is not None)
+        # test base url
+        assert "https://dumps.wikimedia.org/enwiki/"\
+            in dump_url
 
-    def test_get_list_url_wikidump(self):
+    def test_format_date_wikidump_url(self):
         dump_url = get_wikidump_url()
-        list_url_wikidump = get_list_url_wikidump(
+        # test format url
+        latest_date = dump_url.split('/')[-2]
+        assert (len(latest_date) == 8 and latest_date.isdigit())
+
+    def test_get_list_downloads_wikidump(self):
+        dump_url = get_wikidump_url()
+        list_url_wikidump = get_list_downloads_wikidump(
             dump_url=dump_url
         )
         assert list_url_wikidump
+
+    def test_list_url_format(self):
+        dump_url = get_wikidump_url()
+        list_url_wikidump = get_list_downloads_wikidump(
+            dump_url=dump_url
+            )
+
+        def check_format(url_info):
+            url = url_info[0]
+            return (
+                ('pages-articles' in url)
+                and ('xml-p' in url)
+                and ('multistream' not in url)
+            )
+        assert [
+            url for url in list_url_wikidump if check_format(url)
+        ]
+
+    def test_unicity_list_url(self):
+        dump_url = get_wikidump_url()
+        list_url_wikidump = get_list_downloads_wikidump(
+            dump_url=dump_url
+            )
+        urls = [url_info[0] for url_info in list_url_wikidump]
+        assert len(urls) == len(list(set(urls)))
